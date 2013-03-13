e958503ebbb18e487d5910cfe96a6c4627571ee4
fzerorubigd <fzerorubigd@gmail.com>
2013-03-13 12:56:15 +0330

    Adding initial files

---
+++ README.md
@@ -0,0 +1,18 @@
+# PHPThumbServiceProvider
+
+A PHPThumb ServiceProvider for [Silex](http://silex.sensiolabs.org) base on [PHPThumb](http://phpthumb.gxdlabs.com/)
+
+## Usage
+
+### Getting the thumb generator object
+
+```php
+$app->register(new fzerorubigd\PHPThumbServiceProvider());
+```
+
+```php
+$provider = $app['phpthumb'];
+
+$thumb = $provider($fileName);
+$thumb->rotateImage('CW'); // See PHPThumb documents
+$thumb->show();
---
+++ src/fzerorubigd/Silex/PHPThumbServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of PHPThumbServiceProvider.
+ *
+ * (c) fzerorubigd <me@cyberrabbits.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace fzerorubigd\Silex;
+
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+
+class PHPThumbServiceProvider implements ServiceProviderInterface
+{
+    public function register(Application $app)
+    {
+		$app['phpthumb'] = $app->protect(
+			function ($fileName, $options = array(), PluginCollection $plugins = null) {
+				return new PHPThumb\GD($fileName, $options, $plugins);
+			}
+		);
+    }
+
+    public function boot(Application $app)
+    {
+    }
+}
